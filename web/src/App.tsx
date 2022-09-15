423f27ab1b4a453b27d61a30ad2b8061ef21f93f
Caio Haruo <caio.haruo@hotmail.com>
2022-09-15 15:51:08 -0300

    aula #04

---
+++ web/src/App.tsx
@@ -0,0 +1,33 @@
+import './styles/main.css';
+
+import logo from './assets/logo-nlw-esports.svg';
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
+
+function App() {
+  return (
+    <div className="max-w-[1344px] mx-auto my-20 flex flex-col items-center">
+      <img src={logo} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <GameBanner
+          bannerUrl="/game-1.png"
+          title="League of Legends"
+          adsCount={5}
+        />
+      </div>
+
+      <CreateAdBanner />
+    </div>
+  );
+}
+
+export default App;
